b8dfb9d977d6baee9a654423f4e2219331126e30
Ayush saini <aayush.saini4141@gmail.com>
2024-03-28 21:37:28 +0530

    first commit

---
+++ layout/NoxfolioLayout.js
@@ -0,0 +1,29 @@
+"use client";
+import { noxfolioUtilits } from "@/utility";
+import { useEffect } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import { data } from "isotope-layout";
+
+const NoxfolioLayout = ({
+  children,
+  header,
+  footer,
+  noFooter,
+  onePageMenu,
+}) => {
+  useEffect(() => {
+    noxfolioUtilits.animaiton();
+  }, []);
+
+  return (
+    <div className="page-wrapper">
+      <Header header={header} onePageMenu={onePageMenu} />
+      <SideBar />
+      {children}
+      {!noFooter && <Footer footer={footer} />}
+    </div>
+  );
+};
+export default NoxfolioLayout;
